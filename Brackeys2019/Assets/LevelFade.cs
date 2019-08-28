a49416d9714335b749ba30ff9ff3092a309ed2c3
malcolmbec <35633689+malcolmbec@users.noreply.github.com>
2019-08-27 22:01:31 -0400

    LevelFade prefab to fade in to scene/fade out to next scene

---
+++ Brackeys2019/Assets/LevelFade.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelFade : MonoBehaviour
+{
+    public Animator anim;
+    public string levelToLoad;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            FadeToLevel(levelToLoad);
+        }
+    }
+
+    // Fade to level specified by name
+    public void FadeToLevel(string levelName)
+    {
+        levelToLoad = levelName;
+        anim.SetTrigger("FadeOut");
+    }
+
+    // Fade to next level in build index
+    public void FadeToNextLevel()
+    {
+        Scene nextScene = SceneManager.GetSceneByBuildIndex(SceneManager.GetActiveScene().buildIndex + 1);
+        FadeToLevel(nextScene.name);
+    }
+
+    // Load the new scene once the fade is complete
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+}
